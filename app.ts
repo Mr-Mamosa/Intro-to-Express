ef7430dfe99adc574fc4bd365f48580b041ec2b1
Adnan Khan <adnankhan04258@gmail.com>
2025-02-10 12:36:16 +0530

    add bootstrap and font-awesome support

--- app.ts
+++ app.ts
@@ -3,13 +3,9 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import indexRouter from './routes/index';
 
-import indexRouter from './routes/index ';
-  
-
-
-module.exports = app; // If using CommonJS
-const app = express();
+const app = express(); // Declare app before exporting
 export default app;
 
 // View engine setup
@@ -20,21 +16,21 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'node_modules')));  
 
 app.use('/', indexRouter);
 
 // Catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // Error handler
-app.use(function (err: createError.HttpError, req: express.Request, res: express.Response, next: express.NextFunction) {
+app.use((err: createError.HttpError, req: express.Request, res: express.Response, next: express.NextFunction) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   res.status(err.status || 500);
   res.render('error');
 });
-
--- routes/index.ts
+++ routes/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+const router = express.Router(); // Declare router
+
 /* GET home page. */
 export default router;
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// module. exports = router;
+module. exports = router;
