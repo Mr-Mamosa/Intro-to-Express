8fc383eaf3d6aa312a262e1189d811993e2a4c4f
Adnan Khan <adnankhan04258@gmail.com>
2025-02-12 00:05:34 +0530

    added simple Navigation

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,22 @@ const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 exports.default = router;
 router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Home' });
+});
+router.get('/home', function (req, res, next) {
+    res.render('index', { title: 'Home' });
+});
+router.get('/about', function (req, res, next) {
+    res.render('index', { title: 'about' });
+});
+router.get('/projects', function (req, res, next) {
+    res.render('index', { title: 'Projects' });
+});
+router.get('/services', function (req, res, next) {
+    res.render('index', { title: 'services' });
+});
+router.get('/contact', function (req, res, next) {
+    res.render('index', { title: 'Contact' });
 });
 module.exports = router;
 //# sourceMappingURL=index.js.map
--- routes/index.ts
+++ routes/index.ts
@@ -1,10 +1,44 @@
 import express from 'express';
 const router = express.Router(); // Declare router
+export default router;
 
 /* GET home page. */
-export default router;
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next) 
+{
+  res.render('index', { title: 'Home' });
 });
+
+router.get('/home', function(req, res, next) 
+{
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/about', function(req, res, next) 
+{
+  res.render('index', { title: 'about' });
+});
+
+
+router.get('/projects', function(req, res, next) 
+{
+  res.render('index', { title: 'Projects' });
+});
+
+
+router.get('/services', function(req, res, next) 
+{
+  res.render('index', { title: 'services' });
+});
+
+
+router.get('/contact', function(req, res, next) 
+{
+  res.render('index', { title: 'Contact' });
+});
+
+
+
+
+
 
 module. exports = router;
